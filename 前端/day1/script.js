7a3f3763b2cb669082001c6771ef4073c0cabba8
MasterJing <870869624@qq.com>
2025-12-22 09:47:04 +0800

    ``` feat(day1): 更新页面交互逻辑与脚本功能
    将原有的简单文本替换功能更新为动态展示时间与文本内容的功能。
    新增了输入框与时间显示按钮，并通过 script.js 实现更复杂的 HTML 动态生成逻辑。
    修改了元素 ID 并增强了 JavaScript 脚本功能，支持字符串分割与模板字符串渲染。
    ```

--- 前端/day1/script.js
+++ 前端/day1/script.js
@@ -1,5 +1,16 @@
-function Myfunc(){
-    var el = document.getElementById("myText");
-    var x = el.textContent !== undefined ? el.textContent : el.value;
-    document.getElementById("demo").innerHTML = x;
+var text = "Hello World!";
+var text2 = "Hello JavaScript!";
+
+var text3 = text2.split(" ");
+
+var html = `<h1>${text}</h1><ul>`;
+
+for (const x of text3) {
+    html += `<li>${x}</li>`;
+}
+html += `</ul>`;
+
+
+function displayDate() {
+    document.getElementById("myText2").innerHTML = html;
 }
---
+++ 前端/day2/script.js
@@ -0,0 +1,4 @@
+var x =0.2 + 0.1;
+function displayDate() {
+    document.getElementById("myText2").innerHTML = x;
+}
